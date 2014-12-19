2ff85fbffda5883e328533b5c9e6e83fe6c411e9
Alexey Kuznetsov <axet@me.com>
2014-12-19 16:44:05 +0300

    add wpa binaries

--- mt6592-vendor.mk
+++ mt6592-vendor.mk
@@ -2,12 +2,14 @@ PRODUCT_COPY_FILES += \
     vendor/mediatek/mt6592/lib/egl/libGLESv1_CM_mali.so:system/lib/egl/libGLESv1_CM_mali.so \
     vendor/mediatek/mt6592/lib/egl/libGLESv2_mali.so:system/lib/egl/libGLESv2_mali.so \
     vendor/mediatek/mt6592/lib/egl/libEGL_mali.so:system/lib/egl/libEGL_mali.so \
-    vendor/mediatek/mt6592/lib/hw/libaudio.usb.default.so:system/hw/libaudio.usb.default.so \
     vendor/mediatek/mt6592/lib/hw/hwcomposer.mt6592.so:system/lib/hw/hwcomposer.mt6592.so \
     vendor/mediatek/mt6592/lib/hw/gralloc.mt6592.so:system/lib/hw/gralloc.mt6592.so \
-    vendor/mediatek/mt6592/lib/hw/libaudio.r_submix.default.so:system/lib/hw/libaudio.r_submix.default.so \
     vendor/mediatek/mt6592/lib/libdpframework.so:system/lib/libdpframework.so \
     vendor/mediatek/mt6592/lib/libion.so:system/lib/libion.so \
     vendor/mediatek/mt6592/lib/libMali.so:system/lib/libMali.so \
-    vendor/mediatek/mt6592/lib/libm4u.so:system/lib/libm4u.so
+    vendor/mediatek/mt6592/lib/libm4u.so:system/lib/libm4u.so \
+    vendor/mediatek/mt6592/bin/6620_wmt_lpbk:system/bin/6620_wmt_lpbk \
+    vendor/mediatek/mt6592/bin/6620_launcher:system/bin/6620_launcher \
+    vendor/mediatek/mt6592/bin/6620_wmt_concurrency:system/bin/6620_wmt_concurrency \
+    vendor/mediatek/mt6592/bin/wmt_loader:system/bin/wmt_loader
 
