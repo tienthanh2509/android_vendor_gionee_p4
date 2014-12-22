f53ab178aa10f9389adae21985d73938924317f1
Alexey Kuznetsov <axet@me.com>
2014-12-22 16:06:47 +0300

    add gapps

--- mt6592-vendor.mk
+++ mt6592-vendor.mk
@@ -1,38 +1,10 @@
-PRODUCT_COPY_FILES += \
-    vendor/mediatek/mt6592/lib/egl/libGLESv1_CM_mali.so:system/lib/egl/libGLESv1_CM_mali.so \
-    vendor/mediatek/mt6592/lib/egl/libGLESv2_mali.so:system/lib/egl/libGLESv2_mali.so \
-    vendor/mediatek/mt6592/lib/egl/libEGL_mali.so:system/lib/egl/libEGL_mali.so \
-    vendor/mediatek/mt6592/lib/hw/hwcomposer.mt6592.so:system/lib/hw/hwcomposer.mt6592.so \
-    vendor/mediatek/mt6592/lib/hw/gralloc.mt6592.so:system/lib/hw/gralloc.mt6592.so \
-    vendor/mediatek/mt6592/lib/libdpframework.so:system/lib/libdpframework.so \
-    vendor/mediatek/mt6592/lib/libion.so:system/lib/libion.so \
-    vendor/mediatek/mt6592/lib/libMali.so:system/lib/libMali.so \
-    vendor/mediatek/mt6592/lib/libm4u.so:system/lib/libm4u.so \
-    vendor/mediatek/mt6592/bin/6620_wmt_lpbk:system/bin/6620_wmt_lpbk \
-    vendor/mediatek/mt6592/bin/6620_launcher:system/bin/6620_launcher \
-    vendor/mediatek/mt6592/bin/6620_wmt_concurrency:system/bin/6620_wmt_concurrency \
-    vendor/mediatek/mt6592/bin/wmt_loader:system/bin/wmt_loader \
-    vendor/mediatek/mt6592/etc/firmware/modem_1_wg_n.img:system/etc/firmware/modem_1_wg_n.img \
-    vendor/mediatek/mt6592/etc/firmware/WIFI_RAM_CODE_E6:system/etc/firmware/WIFI_RAM_CODE_E6 \
-    vendor/mediatek/mt6592/etc/firmware/mt6572_82_patch_e1_0_hdr.bin:system/etc/firmware/mt6572_82_patch_e1_0_hdr.bin \
-    vendor/mediatek/mt6592/etc/firmware/S_ANDRO_SFL.ini:system/etc/firmware/S_ANDRO_SFL.ini \
-    vendor/mediatek/mt6592/etc/firmware/mt6627/mt6627_fm_v2_coeff.bin:system/etc/firmware/mt6627/mt6627_fm_v2_coeff.bin \
-    vendor/mediatek/mt6592/etc/firmware/mt6627/mt6627_fm_v5_patch.bin:system/etc/firmware/mt6627/mt6627_fm_v5_patch.bin \
-    vendor/mediatek/mt6592/etc/firmware/mt6627/mt6627_fm_v4_patch.bin:system/etc/firmware/mt6627/mt6627_fm_v4_patch.bin \
-    vendor/mediatek/mt6592/etc/firmware/mt6627/mt6627_fm_v1_patch.bin:system/etc/firmware/mt6627/mt6627_fm_v1_patch.bin \
-    vendor/mediatek/mt6592/etc/firmware/mt6627/mt6627_fm_v3_coeff.bin:system/etc/firmware/mt6627/mt6627_fm_v3_coeff.bin \
-    vendor/mediatek/mt6592/etc/firmware/mt6627/mt6627_fm_v3_patch.bin:system/firmware/mt6627/mt6627_fm_v3_patch.bin \
-    vendor/mediatek/mt6592/etc/firmware/mt6627/mt6627_fm_v5_coeff.bin:system/etc/firmware/mt6627/mt6627_fm_v5_coeff.bin \
-    vendor/mediatek/mt6592/etc/firmware/mt6627/mt6627_fm_v2_patch.bin:system/etc/firmware/mt6627/mt6627_fm_v2_patch.bin \
-    vendor/mediatek/mt6592/etc/firmware/mt6627/mt6627_fm_v4_coeff.bin:system/etc/firmware/mt6627/mt6627_fm_v4_coeff.bin \
-    vendor/mediatek/mt6592/etc/firmware/mt6627/mt6627_fm_v1_coeff.bin:system/etc/firmware/mt6627/mt6627_fm_v1_coeff.bin \
-    vendor/mediatek/mt6592/etc/firmware/catcher_filter_1_wg_n.bin:system/etc/firmware/catcher_filter_1_wg_n.bin \
-    vendor/mediatek/mt6592/etc/firmware/WIFI_RAM_CODE_MT6582:system/etc/firmware/WIFI_RAM_CODE_MT6582 \
-    vendor/mediatek/mt6592/etc/firmware/WIFI_RAM_CODE_MT6628:system/etc/firmware/WIFI_RAM_CODE_MT6628 \
-    vendor/mediatek/mt6592/etc/firmware/mt6572_82_patch_e1_1_hdr.bin:system/etc/firmware/mt6572_82_patch_e1_1_hdr.bin \
-    vendor/mediatek/mt6592/etc/firmware/WIFI_RAM_CODE:system/etc/firmware/WIFI_RAM_CODE \
-    vendor/mediatek/mt6592/etc/firmware/WMT_SOC.cfg:system/etc/firmware/WMT_SOC.cfg \
-    vendor/mediatek/mt6592/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-    vendor/mediatek/mt6592/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    vendor/mediatek/mt6592/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/mt6592/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/mt6592/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/mt6592/etc/firmware,system/etc/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/mt6592/etc/wifi,system/etc/wifi)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/mt6592/gapps/system,system)
 
