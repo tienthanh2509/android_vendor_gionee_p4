1bc2b9922e2564d4ab9ab7193be6bac5d06fcbef
Alexey Kuznetsov <axet@me.com>
2015-01-22 09:16:38 +0300

    update vendor file

--- get_vendor.sh
+++ get_vendor.sh
@@ -3,20 +3,34 @@
 SOURCE=$1
 TARGET=.
 
-if [ -z $SOURCE ]; then
-  echo $0 /mnt
-  exit 1
-fi
+ROOT="/etc/firmware/ /etc/wifi/"
 
-FILES="/etc/firmware/ /bin/6620_wmt_lpbk /bin/6620_launcher /bin/6620_wmt_concurrency /bin/wmt_loader \
-/etc/wifi/ /lib/egl/libGLESv1_CM_mali.so /lib/egl/libGLESv2_mali.so /lib/egl/libEGL_mali.so \
+SYSTEM="/bin/6620_wmt_lpbk /bin/6620_launcher /bin/6620_wmt_concurrency /bin/wmt_loader \
+/lib/egl/libGLESv1_CM_mali.so /lib/egl/libGLESv2_mali.so /lib/egl/libEGL_mali.so \
 /lib/libm4u.so /lib/hw/hwcomposer.mt6592.so /lib/hw/gralloc.mt6592.so \
 /lib/libdpframework.so /lib/libion.so /lib/libMali.so"
 
-for FILE in $FILES; do
+# get data from a device
+if [ -z $SOURCE ]; then
+  for FILE in $ROOT ; do
+    T=$TARGET/$FILE
+    adb pull $FILE $T
+  done
+  for FILE in $SYSTEM ; do
+    T=$TARGET/$FILE
+    adb pull /system/$FILE $T
+  done
+
+  exit 0
+fi
+
+# get data from folder
+for FILE in $ROOT $SYTEM ; do
   S=$SOURCE/$FILE
   T=$TARGET/$FILE
   mkdir -p $(dirname $T) || exit 1
   rsync -av --delete $S $T || exit 1
+
+  exit 0
 done
 
