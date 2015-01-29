d9c44618272fde1c4e4ff7e081d5573c2fb6a4e9
Alexey Kuznetsov <axet@me.com>
2015-01-29 16:58:47 +0300

    update firmware

--- get_vendor.sh
+++ get_vendor.sh
@@ -8,7 +8,7 @@ ROOT="/etc/firmware/ /etc/wifi/"
 SYSTEM="/bin/6620_wmt_lpbk /bin/6620_launcher /bin/6620_wmt_concurrency /bin/wmt_loader \
 /lib/egl/libGLESv1_CM_mali.so /lib/egl/libGLESv2_mali.so /lib/egl/libEGL_mali.so \
 /lib/libm4u.so /lib/hw/hwcomposer.mt6592.so /lib/hw/gralloc.mt6592.so \
-/lib/libdpframework.so /lib/libion.so /lib/libMali.so"
+/lib/libdpframework.so /lib/libion.so /lib/libMali.so /lib/mtk-ril.so /lib/librilmtk.so /lib/libaed.so"
 
 # get data from a device
 if [ -z $SOURCE ]; then
