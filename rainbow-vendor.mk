32ff79dcbf859caa67d4a58672cfb720e472ec3d
Al3XKOoL <moonrotation@gmail.com>
2015-04-02 08:25:08 +0100

    Switch to Wiko Rainbow

---
+++ rainbow-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/s5501/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/s5501/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/s5501/etc/firmware,system/etc/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/s5501/etc/wifi,system/etc/wifi)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/s5501/gapps/system,system)
+
