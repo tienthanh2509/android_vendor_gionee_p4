a84280fb7e5f1f81e7c28c6503e837a02b456831
Al3XKOoL <moonrotation@gmail.com>
2015-04-02 14:21:20 +0100

    Fixes

--- rainbow-vendor.mk
+++ rainbow-vendor.mk
@@ -2,9 +2,11 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/lib,sy
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/etc/firmware,system/etc/firmware)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/etc/wifi,system/etc/wifi)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/xbin,system/xbin)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/gapps/system,system)
 
