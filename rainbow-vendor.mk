8a2ffc52fc894e18ca793855877897dbd33bc8c6
Al3XKOoL <moonrotation@gmail.com>
2015-04-02 08:27:35 +0100

    Fix: Switch to Wiko Rainbow

--- rainbow-vendor.mk
+++ rainbow-vendor.mk
@@ -1,10 +1,10 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/s5501/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/s5501/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/s5501/etc/firmware,system/etc/firmware)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/etc/firmware,system/etc/firmware)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/s5501/etc/wifi,system/etc/wifi)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/etc/wifi,system/etc/wifi)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mediatek/s5501/gapps/system,system)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/rainbow/gapps/system,system)
 
